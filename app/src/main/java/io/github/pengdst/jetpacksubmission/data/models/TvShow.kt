03b387eb11fe345a31cdd8278afc41baa68b5265
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-05-11 13:22:23 +0700

    Init commit

---
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/data/models/TvShow.kt
@@ -0,0 +1,17 @@
+package io.github.pengdst.jetpacksubmission.data.models
+
+/**
+ * Created on 5/11/21 by Pengkuh Dwi Septiandi (@pengdst)
+ *
+ * - Github https://github.com/pengdst
+ * - Gitlab https://gitlab.com/pengdst
+ * - LinkedIn https://linkedin.com/in/pengdst
+ */
+data class TvShow(
+    val title: String,
+    val imageUrl: String,
+    val releaseDate: String,
+    val language: String,
+    val genre: String,
+    val storyLine: String,
+)
---
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.pengdst.jetpacksubmission.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.pengdst.jetpacksubmission.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
