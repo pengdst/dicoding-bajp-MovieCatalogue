72401b1250849735336297c6663c0a43001129c4
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-05-19 08:33:32 +0700

    setup hilt android app

---
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/core/JetpackSubmissionApp.kt
@@ -0,0 +1,16 @@
+package io.github.pengdst.jetpacksubmission.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created on 5/19/21 by Pengkuh Dwi Septiandi (@pengdst)
+ *
+ * - Github https://github.com/pengdst
+ * - Gitlab https://gitlab.com/pengdst
+ * - LinkedIn https://linkedin.com/in/pengdst
+ */
+
+@HiltAndroidApp
+class JetpackSubmissionApp : Application() {
+}
