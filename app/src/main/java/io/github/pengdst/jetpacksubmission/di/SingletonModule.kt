62b2896670b3951915d43f1cb492fcf8ae8743c6
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-05-19 19:52:35 +0700

    create SingletonModule.kt

---
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/di/SingletonModule.kt
@@ -0,0 +1,38 @@
+package io.github.pengdst.jetpacksubmission.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.github.pengdst.jetpacksubmission.data.repository.MovieRepository
+import io.github.pengdst.jetpacksubmission.data.repository.MovieRepositoryImpl
+import io.github.pengdst.jetpacksubmission.data.source.remote.RetrofitBuilder
+import io.github.pengdst.jetpacksubmission.data.source.remote.routes.MovieRoute
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+/**
+ * Created on 5/19/21 by Pengkuh Dwi Septiandi (@pengdst)
+ *
+ * - Github https://github.com/pengdst
+ * - Gitlab https://gitlab.com/pengdst
+ * - LinkedIn https://linkedin.com/in/pengdst
+ */
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SingletonModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance() = RetrofitBuilder.build()
+
+    @Provides
+    @Singleton
+    fun provideMovieRoute(retrofit: Retrofit) = retrofit.create(MovieRoute::class.java)
+
+    @Provides
+    @Singleton
+    fun provideMovieRepository(route: MovieRoute): MovieRepository = MovieRepositoryImpl(route)
+
+}
