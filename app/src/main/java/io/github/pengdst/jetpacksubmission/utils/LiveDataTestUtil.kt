002e5f0b1a8c8472d7d2bc89c168facd703d017b
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-05-20 08:47:50 +0700

    create EspressoIdlingResource.kt and LiveDataTestUtil.kt

---
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/utils/LiveDataTestUtil.kt
@@ -0,0 +1,40 @@
+package io.github.pengdst.jetpacksubmission.utils
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created on 5/18/21 by Pengkuh Dwi Septiandi (@pengdst)
+ *
+ * - Github https://github.com/pengdst
+ * - Gitlab https://gitlab.com/pengdst
+ * - LinkedIn https://linkedin.com/in/pengdst
+ */
+
+object LiveDataTestUtil {
+    fun <T> getValue(liveData: LiveData<T>): T {
+        val data = arrayOfNulls<Any>(1)
+        val latch = CountDownLatch(1)
+
+        val observer = object : Observer<T> {
+            override fun onChanged(o: T) {
+                data[0] = o
+                latch.countDown()
+                liveData.removeObserver(this)
+            }
+        }
+
+        liveData.observeForever(observer)
+
+        try {
+            latch.await(2, TimeUnit.SECONDS)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        return data[0] as T
+
+    }
+}
