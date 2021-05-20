6fa2c31acdf4e41c6aebba0b2de2c6786d4739be
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-05-21 01:51:57 +0700

    hardcoded expected test result

--- app/src/androidTest/java/io/github/pengdst/jetpacksubmission/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/io/github/pengdst/jetpacksubmission/ui/home/HomeActivityTest.kt
@@ -11,6 +11,8 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.github.pengdst.jetpacksubmission.R
+import io.github.pengdst.jetpacksubmission.data.source.domain.models.Movie
+import io.github.pengdst.jetpacksubmission.data.source.domain.models.TvShow
 import io.github.pengdst.jetpacksubmission.utils.DataStore
 import io.github.pengdst.jetpacksubmission.utils.EspressoIdlingResource
 import org.junit.After
@@ -33,8 +35,34 @@ class HomeActivityTest {
     @get:Rule
     var activityRule = ActivityScenarioRule(HomeActivity::class.java)
 
-    private val dummyMovie = DataStore.movies
-    private val dummyTvShow = DataStore.tvShowList
+    private val dummyMovie = DataStore.moviesResponse.map { data ->
+        Movie(
+            id = data.id.toString(),
+            title = data.title.toString(),
+            backdropPath = data.backdropPath.toString(),
+            posterPath = data.posterPath.toString(),
+            releaseDate = data.releaseDate.toString(),
+            language = data.spokenLanguages?.map { it.englishName }.toString()
+                .replace("[", "").replace("]", ""),
+            genre = data.genres?.map { it.name }.toString().replace("[", "")
+                .replace("]", ""),
+            storyLine = data.overview.toString()
+        )
+    }
+    private val dummyTvShow = DataStore.tvShowListResponse.map {data->
+        TvShow(
+            id = data.id.toString(),
+            title = data.name.toString(),
+            backdropPath = data.backdropPath.toString(),
+            posterPath = data.posterPath.toString(),
+            releaseDate = data.firstAirDate.toString(),
+            language = data.spokenLanguages?.map { it.englishName }.toString()
+                .replace("[", "").replace("]", ""),
+            genre = data.genres?.map { it.name }.toString().replace("[", "")
+                .replace("]", ""),
+            storyLine = data.overview.toString()
+        )
+    }
 
     @Before
     fun setUp(){
@@ -68,11 +96,11 @@ class HomeActivityTest {
         checkDisplayed(R.id.tv_storyline)
         checkDisplayed(R.id.tv_language)
 
-        checkContent(R.id.tv_title, dummyMovie[0].title)
-        checkContent(R.id.tv_release_date, dummyMovie[0].releaseDate)
-        checkContent(R.id.tv_genre, dummyMovie[0].genre)
-        checkContent(R.id.tv_storyline, dummyMovie[0].storyLine)
-        checkContent(R.id.tv_language, dummyMovie[0].language)
+        checkContent(R.id.tv_title, "Godzilla vs. Kong")
+        checkContent(R.id.tv_release_date, "2021-03-24")
+        checkContent(R.id.tv_genre, "Science Fiction, Action, Drama")
+        checkContent(R.id.tv_language, "English, Turkish")
+        checkContent(R.id.tv_storyline, "In a time when monsters walk the Earth, humanity’s fight for its future sets Godzilla and Kong on a collision course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.")
     }
 
     @Test
@@ -101,11 +129,11 @@ class HomeActivityTest {
         checkDisplayed(R.id.tv_storyline)
         checkDisplayed(R.id.tv_language)
 
-        checkContent(R.id.tv_title, dummyTvShow[0].title)
-        checkContent(R.id.tv_release_date, dummyTvShow[0].releaseDate)
-        checkContent(R.id.tv_genre, dummyTvShow[0].genre)
-        checkContent(R.id.tv_storyline, dummyTvShow[0].storyLine)
-        checkContent(R.id.tv_language, dummyTvShow[0].language)
+        checkContent(R.id.tv_title, "The Flash")
+        checkContent(R.id.tv_release_date, "2014-10-07")
+        checkContent(R.id.tv_genre, "Drama, Sci-Fi & Fantasy")
+        checkContent(R.id.tv_language, "English")
+        checkContent(R.id.tv_storyline, "After a particle accelerator causes a freak storm, CSI Investigator Barry Allen is struck by lightning and falls into a coma. Months later he awakens with the power of super speed, granting him the ability to move through Central City like an unseen guardian angel. Though initially excited by his newfound powers, Barry is shocked to discover he is not the only \"meta-human\" who was created in the wake of the accelerator explosion -- and not everyone is using their new powers for good. Barry partners with S.T.A.R. Labs and dedicates his life to protect the innocent. For now, only a few close friends and associates know that Barry is literally the fastest man alive, but it won't be long before the world learns what Barry Allen has become...The Flash.")
     }
 
     private fun checkScrollToPosition(resId: Int, size: Int) {
