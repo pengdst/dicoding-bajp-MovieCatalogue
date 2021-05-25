b984249383012f1f66d3a524c2ec69dfd5bd6fa9
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-05-25 12:44:42 +0700

    call with coroutine

--- app/src/main/java/io/github/pengdst/jetpacksubmission/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/data/repository/MovieRepositoryImpl.kt
@@ -1,11 +1,9 @@
 package io.github.pengdst.jetpacksubmission.data.repository
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import io.github.pengdst.jetpacksubmission.data.source.domain.models.Movie
-import io.github.pengdst.jetpacksubmission.data.source.domain.models.TvShow
 import io.github.pengdst.jetpacksubmission.data.source.remote.MovieRemoteSource
-import io.github.pengdst.jetpacksubmission.utils.LoadContent
+import io.github.pengdst.jetpacksubmission.data.source.remote.SafeRemoteSource
+import io.github.pengdst.jetpacksubmission.data.source.remote.mapper.MovieMapper.toDomain
+import io.github.pengdst.jetpacksubmission.data.source.remote.mapper.TvMapper.toDomain
 import javax.inject.Inject
 
 /**
@@ -17,62 +15,11 @@ import javax.inject.Inject
  */
 class MovieRepositoryImpl @Inject constructor(
     private val remote: MovieRemoteSource
-) : MovieRepository {
+) : SafeRemoteSource(), MovieRepository {
 
-    override fun getUpcomingMovies(): LiveData<List<Movie>> {
-        val result = MutableLiveData<List<Movie>>()
-
-        remote.getUpcomingMovies(object : LoadContent<List<Movie>>{
-            override fun onContentReceived(courseResponses: List<Movie>?) {
-                courseResponses?.let {
-                    result.postValue(it)
-                }
-            }
-        })
-
-        return result
-    }
-
-    override fun getMovie(movieId: String): LiveData<Movie> {
-        val result = MutableLiveData<Movie>()
-
-        remote.getMovie(movieId, object : LoadContent<Movie>{
-            override fun onContentReceived(courseResponses: Movie?) {
-                courseResponses?.let {
-                    result.postValue(it)
-                }
-            }
-        })
-
-        return result
-    }
-
-    override fun getTvOnAir(): LiveData<List<TvShow>> {
-        val result = MutableLiveData<List<TvShow>>()
-
-        remote.getTvOnAir(object : LoadContent<List<TvShow>>{
-            override fun onContentReceived(courseResponses: List<TvShow>?) {
-                courseResponses?.let {
-                    result.postValue(it)
-                }
-            }
-        })
-
-        return result
-    }
-
-    override fun getTv(tvId: String): LiveData<TvShow> {
-        val result = MutableLiveData<TvShow>()
-
-        remote.getTv(tvId, object : LoadContent<TvShow>{
-            override fun onContentReceived(courseResponses: TvShow?) {
-                courseResponses?.let {
-                    result.postValue(it)
-                }
-            }
-        })
-
-        return result
-    }
+    override suspend fun getUpcomingMovies() = apiCall(remote.getUpcomingMovies())?.results?.toDomain()
+    override suspend fun getMovie(movieId: String) = remote.getMovie(movieId).body()?.toDomain()
+    override suspend fun getTvOnAir() = apiCall(remote.getTvOnAir())?.results?.toDomain()
+    override suspend fun getTv(tvId: String) = apiCall(remote.getTv(tvId))?.toDomain()
 
 }
