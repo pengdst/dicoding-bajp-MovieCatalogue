f7a3d65aa307962e6e037330c3960e4dd9de6918
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-05-25 12:45:51 +0700

    create SafeRemoteSource.kt

---
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/data/source/remote/SafeRemoteSource.kt
@@ -0,0 +1,31 @@
+package io.github.pengdst.jetpacksubmission.data.source.remote
+
+import android.util.Log
+import retrofit2.Response
+
+/**
+ * Created on 5/25/21 by Pengkuh Dwi Septiandi (@pengdst)
+ *
+ * - Github https://github.com/pengdst
+ * - Gitlab https://gitlab.com/pengdst
+ * - LinkedIn https://linkedin.com/in/pengdst
+ */
+abstract class SafeRemoteSource {
+
+    companion object {
+        private const val TAG = "SafeRemoteSource"
+    }
+
+    fun <T> apiCall(response: Response<T>) : T? = try {
+        if (response.isSuccessful) {
+            response.body()
+        } else {
+            Log.e(TAG, "apiCall() called response = $response")
+            null
+        }
+
+    } catch (e: Exception) {
+        Log.e(TAG, "apiCall() called")
+        null
+    }
+}
