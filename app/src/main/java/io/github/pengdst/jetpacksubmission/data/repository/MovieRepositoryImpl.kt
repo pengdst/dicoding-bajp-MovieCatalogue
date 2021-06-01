e9ae66879fd4ef8df26e7979835a9db820889f21
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-01 20:45:32 +0700

    wrap repository result into value object

--- app/src/main/java/io/github/pengdst/jetpacksubmission/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/data/repository/MovieRepositoryImpl.kt
@@ -1,9 +1,13 @@
 package io.github.pengdst.jetpacksubmission.data.repository
 
-import io.github.pengdst.jetpacksubmission.data.remote.source.MovieRemoteSource
-import io.github.pengdst.jetpacksubmission.data.remote.source.SafeRemoteSource
+import android.util.Log
 import io.github.pengdst.jetpacksubmission.data.remote.mapper.MovieMapper.toDomain
 import io.github.pengdst.jetpacksubmission.data.remote.mapper.TvMapper.toDomain
+import io.github.pengdst.jetpacksubmission.data.remote.source.MovieRemoteSource
+import io.github.pengdst.jetpacksubmission.data.remote.source.SafeRemoteSource
+import io.github.pengdst.jetpacksubmission.data.vo.Resource
+import io.github.pengdst.jetpacksubmission.domain.models.Movie
+import io.github.pengdst.jetpacksubmission.domain.models.TvShow
 import io.github.pengdst.jetpacksubmission.domain.repository.MovieRepository
 import javax.inject.Inject
 
@@ -18,9 +22,57 @@ class MovieRepositoryImpl @Inject constructor(
     private val remote: MovieRemoteSource
 ) : SafeRemoteSource(), MovieRepository {
 
-    override suspend fun getUpcomingMovies() = apiCall(remote.getUpcomingMovies())?.results?.toDomain()
-    override suspend fun getMovie(movieId: String) = remote.getMovie(movieId).body()?.toDomain()
-    override suspend fun getTvOnAir() = apiCall(remote.getTvOnAir())?.results?.toDomain()
-    override suspend fun getTv(tvId: String) = apiCall(remote.getTv(tvId))?.toDomain()
+    companion object {
+        private const val TAG = "MovieRepositoryImpl"
+    }
+
+    override suspend fun getUpcomingMovies(): Resource<List<Movie>> {
+        val response = remote.getUpcomingMovies()
+
+        return if (response.isSuccessful) {
+            response.body()?.results?.toDomain()?.let {
+                Resource.Success(it, response.message().toString())
+            } ?: Resource.Error(response.body()?.results?.toDomain(), "Resource Null or Empty")
+        } else {
+            Log.e(TAG, "apiCall() called response = $response")
+            Resource.Error(response.body()?.results?.toDomain(), response.message())
+        }
+    }
+    override suspend fun getMovie(movieId: String): Resource<Movie> {
+        val response = remote.getMovie(movieId)
+
+        return if (response.isSuccessful) {
+            response.body()?.toDomain()?.let {
+                Resource.Success(it, response.message().toString())
+            } ?: Resource.Error(response.body()?.toDomain(), "Resource Null or Empty")
+        } else {
+            Log.e(TAG, "apiCall() called response = $response")
+            Resource.Error(response.body()?.toDomain(), response.message())
+        }
+    }
+    override suspend fun getTvOnAir(): Resource<List<TvShow>> {
+        val response = remote.getTvOnAir()
+
+        return if (response.isSuccessful) {
+            response.body()?.results?.toDomain()?.let {
+                Resource.Success(it, response.message().toString())
+            } ?: Resource.Error(response.body()?.results?.toDomain(), "Resource Null or Empty")
+        } else {
+            Log.e(TAG, "apiCall() called response = $response")
+            Resource.Error(response.body()?.results?.toDomain(), response.message())
+        }
+    }
+    override suspend fun getTv(tvId: String): Resource<TvShow> {
+        val response = remote.getTv(tvId)
+
+        return if (response.isSuccessful) {
+            response.body()?.toDomain()?.let {
+                Resource.Success(it, response.message().toString())
+            } ?: Resource.Error(response.body()?.toDomain(), "Resource Null or Empty")
+        } else {
+            Log.e(TAG, "apiCall() called response = $response")
+            Resource.Error(response.body()?.toDomain(), response.message())
+        }
+    }
 
 }
