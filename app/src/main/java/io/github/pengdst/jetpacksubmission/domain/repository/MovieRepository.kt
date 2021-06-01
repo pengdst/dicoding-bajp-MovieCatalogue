b17680506be4907a1c4a58bebe92d6614abca7e3
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-01 20:45:44 +0700

    wrap repository result into value object

--- app/src/main/java/io/github/pengdst/jetpacksubmission/domain/repository/MovieRepository.kt
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/domain/repository/MovieRepository.kt
@@ -1,5 +1,6 @@
 package io.github.pengdst.jetpacksubmission.domain.repository
 
+import io.github.pengdst.jetpacksubmission.data.vo.Resource
 import io.github.pengdst.jetpacksubmission.domain.models.Movie
 import io.github.pengdst.jetpacksubmission.domain.models.TvShow
 
@@ -12,9 +13,9 @@ import io.github.pengdst.jetpacksubmission.domain.models.TvShow
  */
 interface MovieRepository {
 
-    suspend fun getUpcomingMovies(): List<Movie>?
-    suspend fun getMovie(movieId: String): Movie?
-    suspend fun getTvOnAir(): List<TvShow>?
-    suspend fun getTv(tvId: String): TvShow?
+    suspend fun getUpcomingMovies(): Resource<List<Movie>>
+    suspend fun getMovie(movieId: String): Resource<Movie>
+    suspend fun getTvOnAir(): Resource<List<TvShow>>
+    suspend fun getTv(tvId: String): Resource<TvShow>
 
 }
