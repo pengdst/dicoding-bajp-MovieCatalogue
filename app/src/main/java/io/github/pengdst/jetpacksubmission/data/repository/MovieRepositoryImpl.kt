5727db0ad190c037f15f7613b1efa5d045ad54d2
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-02 11:18:28 +0700

    use network bound resource to show data from local and remote source

--- app/src/main/java/io/github/pengdst/jetpacksubmission/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/data/repository/MovieRepositoryImpl.kt
@@ -1,14 +1,31 @@
 package io.github.pengdst.jetpacksubmission.data.repository
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asFlow
+import io.github.pengdst.jetpacksubmission.data.local.mapper.MovieMapper.toDomain
+import io.github.pengdst.jetpacksubmission.data.local.mapper.MovieMapper.toEntity
+import io.github.pengdst.jetpacksubmission.data.local.mapper.TvShowMapper.toDomain
+import io.github.pengdst.jetpacksubmission.data.local.mapper.TvShowMapper.toEntity
+import io.github.pengdst.jetpacksubmission.data.local.room.model.MovieEntity
+import io.github.pengdst.jetpacksubmission.data.local.room.model.TvShowEntity
+import io.github.pengdst.jetpacksubmission.data.local.source.MovieLocalSource
+import io.github.pengdst.jetpacksubmission.data.remote.helpers.ApiResponse
 import io.github.pengdst.jetpacksubmission.data.remote.mapper.MovieMapper.toDomain
 import io.github.pengdst.jetpacksubmission.data.remote.mapper.TvMapper.toDomain
+import io.github.pengdst.jetpacksubmission.data.remote.retrofit.models.MovieDto
+import io.github.pengdst.jetpacksubmission.data.remote.retrofit.models.TvDto
+import io.github.pengdst.jetpacksubmission.data.remote.retrofit.response.MovieResponse
+import io.github.pengdst.jetpacksubmission.data.remote.retrofit.response.TvResponse
 import io.github.pengdst.jetpacksubmission.data.remote.source.MovieRemoteSource
 import io.github.pengdst.jetpacksubmission.data.remote.source.SafeRemoteSource
+import io.github.pengdst.jetpacksubmission.data.utils.NetworkBoundResource
 import io.github.pengdst.jetpacksubmission.data.vo.Resource
 import io.github.pengdst.jetpacksubmission.domain.models.Movie
 import io.github.pengdst.jetpacksubmission.domain.models.TvShow
 import io.github.pengdst.jetpacksubmission.domain.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 /**
@@ -19,60 +36,155 @@ import javax.inject.Inject
  * - LinkedIn https://linkedin.com/in/pengdst
  */
 class MovieRepositoryImpl @Inject constructor(
-    private val remote: MovieRemoteSource
+    private val remote: MovieRemoteSource,
+    private val local: MovieLocalSource
 ) : SafeRemoteSource(), MovieRepository {
 
-    companion object {
-        private const val TAG = "MovieRepositoryImpl"
+    override fun getUpcomingMovies(): LiveData<Resource<List<Movie>>> {
+        return object : NetworkBoundResource<List<Movie>, List<MovieEntity>, MovieResponse>() {
+            override suspend fun fetchFromLocal(): Flow<List<MovieEntity>> {
+                return local.getAllMovies().asFlow()
+            }
+
+            override fun shouldFetchFromRemote(db: List<MovieEntity>?) = true
+
+            override suspend fun fetchFromRemote(): Flow<ApiResponse<MovieResponse>?> {
+                val response = remote.getUpcomingMovies()
+                return flow {
+                    if (response.isSuccessful) {
+                        response.body()?.let {
+                            emit(ApiResponse.success(it))
+                        } ?: run {
+                            emit(ApiResponse.error("Response Empty or Null", response.body()))
+                        }
+                    } else {
+                        emit(ApiResponse.error(response.message(), response.body()))
+                    }
+                }.catch { e ->
+                    emit(ApiResponse.error(e.message ?: "Error", response.body(), e))
+                }
+            }
+
+            override fun convertToDomain(db: List<MovieEntity>): List<Movie> {
+                return db.toDomain()
+            }
+
+            override suspend fun saveRemoteData(remote: MovieResponse) {
+                remote.results?.toDomain()?.map { it.toEntity() }?.let {
+                    local.saveMovies(it)
+                }
+            }
+        }.liveData
     }
 
-    override suspend fun getUpcomingMovies(): Resource<List<Movie>> {
-        val response = remote.getUpcomingMovies()
+    override fun getMovie(movieId: String): LiveData<Resource<Movie>> {
 
-        return if (response.isSuccessful) {
-            response.body()?.results?.toDomain()?.let {
-                Resource.Success(it, response.message().toString())
-            } ?: Resource.Error(response.body()?.results?.toDomain(), "Resource Null or Empty")
-        } else {
-            Log.e(TAG, "apiCall() called response = $response")
-            Resource.Error(response.body()?.results?.toDomain(), response.message())
-        }
+        return object : NetworkBoundResource<Movie, MovieEntity, MovieDto>() {
+            override suspend fun fetchFromLocal(): Flow<MovieEntity> {
+                return local.getMovie(movieId).asFlow()
+            }
+
+            override fun shouldFetchFromRemote(db: MovieEntity?) = true
+
+            override suspend fun fetchFromRemote(): Flow<ApiResponse<MovieDto>?> {
+                val response = remote.getMovie(movieId)
+                return flow {
+                    if (response.isSuccessful) {
+                        response.body()?.let {
+                            emit(ApiResponse.success(it))
+                        } ?: run {
+                            emit(ApiResponse.error("Response Empty or Null", response.body()))
+                        }
+                    } else {
+                        emit(ApiResponse.error(response.message(), response.body()))
+                    }
+                }.catch { e ->
+                    emit(ApiResponse.error(e.message ?: "Error", response.body(), e))
+                }
+            }
+
+            override fun convertToDomain(db: MovieEntity): Movie {
+                return db.toDomain()
+            }
+
+            override suspend fun saveRemoteData(remote: MovieDto) {
+                local.saveMovie(remote.toDomain().toEntity())
+            }
+        }.liveData
     }
-    override suspend fun getMovie(movieId: String): Resource<Movie> {
-        val response = remote.getMovie(movieId)
 
-        return if (response.isSuccessful) {
-            response.body()?.toDomain()?.let {
-                Resource.Success(it, response.message().toString())
-            } ?: Resource.Error(response.body()?.toDomain(), "Resource Null or Empty")
-        } else {
-            Log.e(TAG, "apiCall() called response = $response")
-            Resource.Error(response.body()?.toDomain(), response.message())
-        }
+    override fun getTvOnAir(): LiveData<Resource<List<TvShow>>> {
+
+        return object : NetworkBoundResource<List<TvShow>, List<TvShowEntity>, TvResponse>() {
+            override suspend fun fetchFromLocal(): Flow<List<TvShowEntity>> {
+                return local.getTvShows().asFlow()
+            }
+
+            override fun shouldFetchFromRemote(db: List<TvShowEntity>?) = true
+
+            override suspend fun fetchFromRemote(): Flow<ApiResponse<TvResponse>?> {
+                val response = remote.getTvOnAir()
+                return flow {
+                    if (response.isSuccessful) {
+                        response.body()?.let {
+                            emit(ApiResponse.success(it))
+                        } ?: run {
+                            emit(ApiResponse.error("Response Empty or Null", response.body()))
+                        }
+                    } else {
+                        emit(ApiResponse.error(response.message(), response.body()))
+                    }
+                }.catch { e ->
+                    emit(ApiResponse.error(e.message ?: "Error", response.body(), e))
+                }
+            }
+
+            override fun convertToDomain(db: List<TvShowEntity>): List<TvShow> {
+                return db.toDomain()
+            }
+
+            override suspend fun saveRemoteData(remote: TvResponse) {
+                remote.results?.toDomain()?.map { it.toEntity() }?.let {
+                    local.saveTvShows(it)
+                }
+            }
+        }.liveData
     }
-    override suspend fun getTvOnAir(): Resource<List<TvShow>> {
-        val response = remote.getTvOnAir()
 
-        return if (response.isSuccessful) {
-            response.body()?.results?.toDomain()?.let {
-                Resource.Success(it, response.message().toString())
-            } ?: Resource.Error(response.body()?.results?.toDomain(), "Resource Null or Empty")
-        } else {
-            Log.e(TAG, "apiCall() called response = $response")
-            Resource.Error(response.body()?.results?.toDomain(), response.message())
-        }
-    }
-    override suspend fun getTv(tvId: String): Resource<TvShow> {
-        val response = remote.getTv(tvId)
+    override fun getTv(tvId: String): LiveData<Resource<TvShow>> {
 
-        return if (response.isSuccessful) {
-            response.body()?.toDomain()?.let {
-                Resource.Success(it, response.message().toString())
-            } ?: Resource.Error(response.body()?.toDomain(), "Resource Null or Empty")
-        } else {
-            Log.e(TAG, "apiCall() called response = $response")
-            Resource.Error(response.body()?.toDomain(), response.message())
-        }
+        return object : NetworkBoundResource<TvShow, TvShowEntity, TvDto>() {
+            override suspend fun fetchFromLocal(): Flow<TvShowEntity> {
+                return local.getTv(tvId).asFlow()
+            }
+
+            override fun shouldFetchFromRemote(db: TvShowEntity?) = true
+
+            override suspend fun fetchFromRemote(): Flow<ApiResponse<TvDto>?> {
+                val response = remote.getTv(tvId)
+                return flow {
+                    if (response.isSuccessful) {
+                        response.body()?.let {
+                            emit(ApiResponse.success(it))
+                        } ?: run {
+                            emit(ApiResponse.error("Response Empty or Null", response.body()))
+                        }
+                    } else {
+                        emit(ApiResponse.error(response.message(), response.body()))
+                    }
+                }.catch { e ->
+                    emit(ApiResponse.error(e.message ?: "Error", response.body(), e))
+                }
+            }
+
+            override fun convertToDomain(db: TvShowEntity): TvShow {
+                return db.toDomain()
+            }
+
+            override suspend fun saveRemoteData(remote: TvDto) {
+                local.saveTvShow(remote.toDomain().toEntity())
+            }
+        }.liveData
     }
 
 }
