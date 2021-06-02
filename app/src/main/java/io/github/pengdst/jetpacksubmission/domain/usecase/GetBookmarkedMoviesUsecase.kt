33baaa5b59df1a0d07108ce01f4361cc2750219a
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-02 23:18:56 +0700

    create GetBookmarkedTvShowsUsecase and GetBookmarkedMoviesUsecase

---
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/domain/usecase/GetBookmarkedMoviesUsecase.kt
@@ -0,0 +1,19 @@
+package io.github.pengdst.jetpacksubmission.domain.usecase
+
+import androidx.paging.PagingData
+import io.github.pengdst.jetpacksubmission.core.UseCase
+import io.github.pengdst.jetpacksubmission.domain.models.Movie
+import io.github.pengdst.jetpacksubmission.domain.repository.MovieRepository
+import javax.inject.Inject
+
+/**
+ * Created on 6/1/21 by Pengkuh Dwi Septiandi (@pengdst)
+ *
+ * - Github https://github.com/pengdst
+ * - Gitlab https://gitlab.com/pengdst
+ * - LinkedIn https://linkedin.com/in/pengdst
+ */
+class GetBookmarkedMoviesUsecase @Inject constructor(private val repository: MovieRepository): UseCase<PagingData<Movie>, GetBookmarkedMoviesUsecase.Companion>() {
+    companion object;
+    override fun run(params: Companion) = repository.getBookmarkedMovies()
+}
---
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/domain/usecase/GetBookmarkedTvShowsUsecase.kt
@@ -0,0 +1,19 @@
+package io.github.pengdst.jetpacksubmission.domain.usecase
+
+import androidx.paging.PagingData
+import io.github.pengdst.jetpacksubmission.core.UseCase
+import io.github.pengdst.jetpacksubmission.domain.models.TvShow
+import io.github.pengdst.jetpacksubmission.domain.repository.MovieRepository
+import javax.inject.Inject
+
+/**
+ * Created on 6/1/21 by Pengkuh Dwi Septiandi (@pengdst)
+ *
+ * - Github https://github.com/pengdst
+ * - Gitlab https://gitlab.com/pengdst
+ * - LinkedIn https://linkedin.com/in/pengdst
+ */
+class GetBookmarkedTvShowsUsecase @Inject constructor(private val repository: MovieRepository): UseCase<PagingData<TvShow>, GetBookmarkedTvShowsUsecase.Companion>() {
+    companion object;
+    override fun run(params: Companion) = repository.getBookmarkedTvShows()
+}
