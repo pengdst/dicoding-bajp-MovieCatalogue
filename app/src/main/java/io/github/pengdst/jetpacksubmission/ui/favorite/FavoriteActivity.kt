bbd370b2f47b0c2ccf94254e5bc692018baa9b8f
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-05 07:13:36 +0700

    replace FavoriteFragment with MovieFavoriteFragment and TvShowFavoriteFragment

--- app/src/main/java/io/github/pengdst/jetpacksubmission/ui/favorite/FavoriteActivity.kt
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/ui/favorite/FavoriteActivity.kt
@@ -1,16 +1,17 @@
 package io.github.pengdst.jetpacksubmission.ui.favorite
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.pengdst.jetpacksubmission.R
 import io.github.pengdst.jetpacksubmission.databinding.ActivityFavoriteBinding
 import io.github.pengdst.jetpacksubmission.domain.models.Section
 import io.github.pengdst.jetpacksubmission.ui.detail.DetailActivity
+import io.github.pengdst.jetpacksubmission.ui.favorite.sections.movie.MovieFavoriteFragment
+import io.github.pengdst.jetpacksubmission.ui.favorite.sections.tv.TvShowFavoriteFragment
 import io.github.pengdst.jetpacksubmission.ui.home.ContentCallback
 import io.github.pengdst.jetpacksubmission.ui.home.sections.SectionsPagerAdapter
-import io.github.pengdst.jetpacksubmission.utils.DataStore
 import io.github.pengdst.libs.ui.activity.viewbinding.ActivityViewBindingDelegate.Companion.viewBindings
 import javax.inject.Inject
 
@@ -26,8 +27,8 @@ class FavoriteActivity : AppCompatActivity(), ContentCallback {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-        sectionsPagerAdapter.addSection(Section(getString(R.string.movie), FavoriteFragment.newInstance(DataStore.TYPE_MOVIE)))
-        sectionsPagerAdapter.addSection(Section(getString(R.string.tv_show), FavoriteFragment.newInstance(DataStore.TYPE_TV_SHOW)))
+        sectionsPagerAdapter.addSection(Section(getString(R.string.movie), MovieFavoriteFragment()))
+        sectionsPagerAdapter.addSection(Section(getString(R.string.tv_show), TvShowFavoriteFragment()))
 
         binding.viewPager.adapter = sectionsPagerAdapter
         binding.tabs.setupWithViewPager(binding.viewPager)
