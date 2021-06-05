e8e134db8e2821ca2634e177008b3fd34ee58003
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-05 07:11:26 +0700

    create FavoriteActivityTest.kt

---
+++ app/src/androidTest/java/io/github/pengdst/jetpacksubmission/ui/favorite/FavoriteActivityTest.kt
@@ -0,0 +1,126 @@
+package io.github.pengdst.jetpacksubmission.ui.favorite
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.pressBack
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import io.github.pengdst.jetpacksubmission.R
+import io.github.pengdst.jetpacksubmission.ui.home.HomeActivity
+import io.github.pengdst.jetpacksubmission.utils.EspressoIdlingResource
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Created on 6/4/21 by Pengkuh Dwi Septiandi (@pengdst)
+ *
+ * - Github https://github.com/pengdst
+ * - Gitlab https://gitlab.com/pengdst
+ * - LinkedIn https://linkedin.com/in/pengdst
+ */
+class FavoriteActivityTest {
+
+    @get:Rule
+    var activityRule = ActivityScenarioRule(HomeActivity::class.java)
+
+    @Before
+    fun setUp(){
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.getEspressoIdlingResourceForMainActivity())
+    }
+
+    @After
+    fun tearDown(){
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.getEspressoIdlingResourceForMainActivity())
+    }
+
+    @Test
+    fun loadMovie() {
+        onView(withText("Movie")).perform(click())
+        checkDisplayed(R.id.rv_movies)
+        checkScrollToPosition(R.id.rv_movies, 5)
+    }
+
+    @Test
+    fun testFavoriteMovie(){
+        loadMovie()
+        onView(withId(R.id.rv_movies)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.fab_favourite)).perform(click())
+        onView(isRoot()).perform(pressBack())
+
+        onView(withId(R.id.menu_favorite)).perform(click())
+        onView(withText("Movie")).perform(click())
+
+        onView(withId(R.id.rv_movies)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_movies)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        onView(withId(R.id.fab_favourite)).perform(click())
+
+        onView(withId(R.id.iv_backdrop)).check(matches(isDisplayed()))
+        onView(withId(R.id.iv_backdrop)).perform(ViewActions.swipeUp())
+        onView(withId(R.id.iv_thumbnail)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_release_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_language)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_genre)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_storyline)).check(matches(isDisplayed()))
+
+        onView(isRoot()).perform(pressBack())
+    }
+
+    @Test
+    fun loadTvShow() {
+        onView(withText("Tv Show")).perform(click())
+        checkDisplayed(R.id.rv_tv_shows)
+        checkScrollToPosition(R.id.rv_tv_shows, 5)
+    }
+
+    @Test
+    fun testFavoriteTvShow(){
+        loadTvShow()
+        onView(withId(R.id.rv_tv_shows)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.fab_favourite)).perform(click())
+        onView(isRoot()).perform(pressBack())
+
+        onView(withId(R.id.menu_favorite)).perform(click())
+        onView(withText("Tv Show")).perform(click())
+
+        onView(withId(R.id.rv_tv_shows)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_tv_shows)).perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        onView(withId(R.id.fab_favourite)).perform(click())
+
+        onView(withId(R.id.iv_backdrop)).check(matches(isDisplayed()))
+        onView(withId(R.id.iv_backdrop)).perform(ViewActions.swipeUp())
+        onView(withId(R.id.iv_thumbnail)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_release_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_language)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_genre)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_storyline)).check(matches(isDisplayed()))
+
+        onView(isRoot()).perform(pressBack())
+    }
+
+    private fun checkScrollToPosition(resId: Int, size: Int) {
+        onView(withId(resId))
+            .perform(scrollToPosition<RecyclerView.ViewHolder>(size))
+    }
+
+    private fun performRecyclerViewClick(resId: Int, position: Int) {
+        onView(withId(resId))
+            .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(position, click()))
+    }
+
+    private fun checkDisplayed(resId: Int) {
+        onView(withId(resId))
+            .check(matches(isDisplayed()))
+    }
+}
