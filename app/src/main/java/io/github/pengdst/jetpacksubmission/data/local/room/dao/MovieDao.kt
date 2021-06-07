0932a6e148f9b2b19e73e40da91bd42c19f09500
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-06-07 09:55:52 +0700

    getMovies and getTvShows order by createdAt

--- app/src/main/java/io/github/pengdst/jetpacksubmission/data/local/room/dao/MovieDao.kt
+++ app/src/main/java/io/github/pengdst/jetpacksubmission/data/local/room/dao/MovieDao.kt
@@ -16,7 +16,7 @@ import io.github.pengdst.jetpacksubmission.data.local.room.model.TvShowEntity
 @Dao
 interface MovieDao {
 
-    @Query("SELECT * FROM movies")
+    @Query("SELECT * FROM movies ORDER BY createdAt")
     fun getMovies(): LiveData<List<MovieEntity>>
 
     @Query("SELECT * FROM movies where isFavourite = 1")
@@ -35,7 +35,7 @@ interface MovieDao {
     @Update
     suspend fun updateMovie(movie: MovieEntity)
 
-    @Query("SELECT * FROM tv_shows")
+    @Query("SELECT * FROM tv_shows ORDER BY createdAt")
     fun getTvShows(): LiveData<List<TvShowEntity>>
 
     @Query("SELECT * FROM tv_shows where isFavourite = 1")
